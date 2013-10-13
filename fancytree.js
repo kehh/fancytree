c3351282a16091774307aa8874ba2c96e2ba6df3
Kehan Harman <kehanharman@gmail.com>
2013-10-13 23:48:06 +0800

    Still work in progress.

--- fancytree.js
+++ fancytree.js
@@ -1,17 +1,19 @@
-(function ($) {
-console.log('loading fancytree');
-Drupal.behaviors.initFancytree = {
-  attach: function (context, settings) {
-    if (!$.isFunction($.fancytree)) {
-      return;
+(function($) {
+  console.log('loading fancytree');
+  Drupal.behaviors.initFancytree = {
+    attach: function(context, settings) {
+      //if (!$.isFunction($.fancytree)) {
+        // return;
+      // }
+      if (!settings.fancytree) {
+        settings.fancytree = {};
+      }
+      if (!settings.selector) {
+        settings.selector = '.fancytree';
+      }
+      console.log(settings);
+      $(settings.selector, context).once('init-fancytree').fancytree(settings.fancytree);
     }
-    if(!settings.fancytree){
-      settings.fancytree = {};
-    }
-    $('.fancytree', context)
-      .once('init-fancytree')
-      .fancytree(settings.fancytree);
-  }
-};
+  };
 
 })(jQuery);
