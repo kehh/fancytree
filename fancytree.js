a088d02730702f2d4e7a64e7deff90a315e6e0f4
Kehan Harman <kehan@gaiaresources.com.au>
2013-10-14 17:47:02 +0800

    Now renders a form with selection and passes some values. Still WIP.

--- fancytree.js
+++ fancytree.js
@@ -3,17 +3,33 @@
   Drupal.behaviors.initFancytree = {
     attach: function(context, settings) {
       //if (!$.isFunction($.fancytree)) {
-        // return;
+      // return;
       // }
       if (!settings.fancytree) {
         settings.fancytree = {};
       }
-      if (!settings.selector) {
-        settings.selector = '.fancytree';
+      if (!settings.fancytree.selector) {
+        settings.fancytree.selector = '.fancytree';
       }
-      console.log(settings);
-      $(settings.selector, context).once('init-fancytree').fancytree(settings.fancytree);
+      if(settings.fancytree.checkbox){
+        settings.fancytree.select = function(e, data){
+          console.log(data);
+          console.log(data.tree.getSelectedNodes());
+          var selected = data.tree.getSelectedNodes();
+          for(i in selected){
+
+          }
+
+          var selKeys = $.map(data.tree.getSelectedNodes(), function(node){
+            return node.key;
+          });
+          console.log(selKeys);
+          $(this).parent('.fancytree-wrapper').find('input').val(JSON.stringify(selKeys));
+          // $("#echoSelection3").text(selKeys.join(", "));
+
+        }
+      }
+      $(settings.fancytree.selector, context).once('init-fancytree').fancytree(settings.fancytree);
     }
   };
-
 })(jQuery);
