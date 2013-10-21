6caabdc6f26c462e79eb26fba0ce1549b63854b2
Kehan Harman <kehan@gaiaresources.com.au>
2013-10-21 18:36:13 +0800

    A working form element. Still need to add support for filtering values

--- fancytree.js
+++ fancytree.js
@@ -1,5 +1,47 @@
 (function($) {
-  console.log('loading fancytree');
+  if(!Drupal.fancytree){
+    Drupal.fancytree = {};
+  }
+  /**
+  * Function to store the selected data
+  */
+  Drupal.fancytree.select = function(e) {
+            var tree = $(e.target).fancytree('getTree');
+            var selKeys = $.map(/*data.*/tree.getSelectedNodes(true), function(node) {
+              return node.getKeyPath();
+            });
+            $(e.target).parent('.fancytree-wrapper').find('input.treevalue').val(JSON.stringify(selKeys));
+          };
+  Drupal.fancytree.create = function(e) {
+          var getTree = function(selector) {
+              return $(selector).fancytree('getTree');
+            };
+          var filterWrapper = $('<div class="filter-wrapper"></div>');
+          var filter = $('<input class="filter" type="text" placeholder="Filter..." />');
+          var reset = $('<button class="reset" disabled="disabled">&times;</button>');
+          reset.click(e.target, function(e, eventdata) {
+            e.preventDefault();
+            filter.val("");
+            getTree(e.data).clearFilter();
+            $(this).attr('disabled', 'disabled');
+
+
+          });
+          filterWrapper.append(filter).append(reset);
+          $(this).prepend(filterWrapper);
+          filter.keyup(e.target, function(e, filterdata) {
+            var tree = getTree(e.data);
+
+            var match = $(this).val();
+            if (e && e.which === $.ui.keyCode.ESCAPE || $.trim(match) === "") {
+              reset.click();
+              return;
+            }
+            // Pass text as filter string (will be matched as substring in the node title)
+            var n = tree.applyFilter(match);
+            reset.attr("disabled", false);
+          }).focus();
+        };
   Drupal.behaviors.initFancytree = {
     attach: function(context, settings) {
       //if (!$.isFunction($.fancytree)) {
@@ -8,28 +50,22 @@
       if (!settings.fancytree) {
         settings.fancytree = {};
       }
-      if (!settings.fancytree.selector) {
-        settings.fancytree.selector = '.fancytree';
-      }
-      if(settings.fancytree.checkbox){
-        settings.fancytree.select = function(e, data){
-          console.log(data);
-          console.log(data.tree.getSelectedNodes());
-          var selected = data.tree.getSelectedNodes();
-          for(i in selected){
 
-          }
-
-          var selKeys = $.map(data.tree.getSelectedNodes(), function(node){
-            return node.key;
-          });
-          console.log(selKeys);
-          $(this).parent('.fancytree-wrapper').find('input').val(JSON.stringify(selKeys));
-          // $("#echoSelection3").text(selKeys.join(", "));
-
+      for (var id in settings.fancytree) {
+        var setting = settings.fancytree[id];
+        setting.create = Drupal.fancytree.create;
+        if (setting.checkbox) {
+          // Initialise the value field with the values that are actually
+          // selected in the tree
+          setting.init = setting.select = Drupal.fancytree.select;
         }
+        var selector = '#' + id;
+        var treeElement = $(selector);
+        console.log(selector);
+        treeElement.once(function(){
+          $(this).fancytree(setting);
+        });
       }
-      $(settings.fancytree.selector, context).once('init-fancytree').fancytree(settings.fancytree);
     }
   };
 })(jQuery);
